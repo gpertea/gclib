63c6120c45b5b26fea92908e0822d9c6e11d6ea7
Geo Pertea <geo.pertea@gmail.com>
2018-05-11 14:42:18 -0400

    setting GCLIB_VERSION for auto-tagging

--- tag_git.sh
+++ tag_git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 git checkout master
-ver=$(fgrep '#define VERSION ' GBase.h)
+ver=$(fgrep '#define GCLIB_VERSION ' GBase.h)
 ver=${ver#*\"}
 ver=${ver%%\"*}
 git tag -a "v$ver" -m "release $ver"
