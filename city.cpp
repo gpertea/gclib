e7dd5c63c4dcb6ade21fe68bbda53faa49f19621
Geo Pertea <geo.pertea@gmail.com>
2020-07-07 22:13:43 -0400

    fix include syntax

--- city.cpp
+++ city.cpp
@@ -27,10 +27,8 @@
 // possible hash functions, by using SIMD instructions, or by
 // compromising on hash quality.
 
-//#include "config.h"
-#include <city.h>
-
-#include <algorithm>
+#include "city.h"
+#include <algorithm> //for std::swap
 #include <string.h>  // for memcpy and memset
 
 using namespace std;
@@ -90,17 +88,17 @@ static uint32 UNALIGNED_LOAD32(const char *p) {
 #elif defined(__MINGW32__)
 // Swap bytes in 32 bit value.
 #define bswap_32(x) \
-     ((((x) & 0xff000000u) >> 24) | (((x) & 0x00ff0000u) >>  8) |             \
+     ((((x) & 0xff000000u) >> 24) | (((x) & 0x00ff0000u) >>  8) | \
       (((x) & 0x0000ff00u) <<  8) | (((x) & 0x000000ffu) << 24))
 
 # define bswap_64(x) \
-     ((((x) & 0xff00000000000000ull) >> 56)                                   \
-      | (((x) & 0x00ff000000000000ull) >> 40)                                 \
-      | (((x) & 0x0000ff0000000000ull) >> 24)                                 \
-      | (((x) & 0x000000ff00000000ull) >> 8)                                  \
-      | (((x) & 0x00000000ff000000ull) << 8)                                  \
-      | (((x) & 0x0000000000ff0000ull) << 24)                                 \
-      | (((x) & 0x000000000000ff00ull) << 40)                                 \
+     ((((x) & 0xff00000000000000ull) >> 56)    \
+      | (((x) & 0x00ff000000000000ull) >> 40)  \
+      | (((x) & 0x0000ff0000000000ull) >> 24)  \
+      | (((x) & 0x000000ff00000000ull) >> 8)   \
+      | (((x) & 0x00000000ff000000ull) << 8)   \
+      | (((x) & 0x0000000000ff0000ull) << 24)  \
+      | (((x) & 0x000000000000ff00ull) << 40)  \
       | (((x) & 0x00000000000000ffull) << 56))
 
 #else
